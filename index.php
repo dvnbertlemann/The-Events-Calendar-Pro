3ba8ffa2ce5293dc557e8c963d370a235626a45b
dvnbertlemann <52090153+dvnbertlemann@users.noreply.github.com>
2019-06-21 18:21:30 -1000

    Testing passing variables
    Adding a variable to be passed to the html file

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<?php include("events-calendar-pro.php"); ?>
 	<link rel="stylesheet" type="text/css" href="style.css">
 <body>
 	<div id="content">
 		<h1>The Events Calendar Pro Plugin</h1>
+		<?php echo("<p>$content</p>"); ?>
 	</div>
 </body>
 </html>
