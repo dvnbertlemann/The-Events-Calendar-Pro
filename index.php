667e19bc68890b82e1f7fdc1ef809ce62e292e36
dvnbertlemann <52090153+dvnbertlemann@users.noreply.github.com>
2019-06-21 17:55:57 -1000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="mystyle.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 <body>
 	<div id="content">
 		<h1>The Events Calendar Pro Plugin</h1>
